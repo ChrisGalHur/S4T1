7099658b327175bcb503e054f27346bdb003ab5e
ChrisGalHur <108464294+ChrisGalHur@users.noreply.github.com>
2023-01-22 19:02:06 +0100

    Nivel 1 hecho

---
+++ src/main/java/cat/itacademy/barcelonactiva/GalvezHurtado/Christian/s04/t01/n01/S04T01N01GalvezHurtadoChristian/S04T01N01GalvezHurtadoChristianApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.GalvezHurtado.Christian.s04.t01.n01.S04T01N01GalvezHurtadoChristian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01GalvezHurtadoChristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01GalvezHurtadoChristianApplication.class, args);
+	}
+
+}
